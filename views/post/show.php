2f29cf6f76c152df24c15e01859b5bef51a5d009
Anatol Kulesh <tola_k@mail.ru>
2018-02-16 15:00:51 +0300

    18 урок

--- views/post/show.php
+++ views/post/show.php
@@ -1,11 +1,22 @@
+<?php 
+// импортируем, то есть подключаем класс виджета
+use app\components\MyWidget;
+ ?>
+
 <h1>Show Action</h1>
+
+<?php // echo MyWidget::widget(['name' => 'Вася']); ?>
+
+<?php MyWidget::begin() ?>
+<h1>привет мир!</h1>
+<?php MyWidget::end() ?>
 
 <?php //debug($cats); ?>
 <?php //echo count($cats[0]->products); ?>
 <?php //debug($cats); ?>
 
 <?php 
-foreach ($cats as $cat) {
+/*foreach ($cats as $cat) {
 	echo '<ul>';
 		echo '<li>'. $cat->title .'</li>';
 		$products = $cat->products;
@@ -15,7 +26,7 @@ foreach ($cats as $cat) {
 			echo '</ul>';
 		}
 	echo '</ul>';
-}
+}*/
  ?>
 
 <button class="btn btn-success" id="btn">Нажми...</button>
