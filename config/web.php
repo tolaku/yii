12a07d0cdd1653e7b23d1b3ebf144044933dd83d
Anatol Kulesh <tola_k@mail.ru>
2018-02-16 23:12:56 +0300

    21 урок

--- config/web.php
+++ config/web.php
@@ -8,7 +8,7 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language' => 'ru',
-    /*'layout' => 'basic',*/
+    'layout' => 'main',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -17,6 +17,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'KRyZb7m2saKizjrWNRmnFwxhLe3fwAmN',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -45,14 +46,21 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'suffix' => '.html',
             'rules' => [
+                /*[
+                    'pattern' => '',
+                    'route' => 'site/index',
+                    'suffix' => ''
+                ],*/
+                '<action:(about|contact|login)>' => 'site/<action>',
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
